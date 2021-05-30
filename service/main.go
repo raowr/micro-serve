7ef94dfe4eb5e43e3c0411a4c8c6d3b2d0fda739
R <r@RdeMacBook-Air.local>
2021-05-30 18:16:56 +0800

    添加文件

---
+++ service/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"micro-service/service/handler"
+
+	"github.com/micro/go-micro/v2/logger"
+
+	"github.com/micro/go-micro/v2"
+
+	//"github.com/micro/go-micro/v2/client/selector"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+
+	post "micro-service/proto/post"
+	user "micro-service/proto/user"
+)
+
+func main() {
+	//reg := etcd.NewRegistry(func(options *registry.Options) {
+	//	options.Addrs = []string{
+	//		"xx.xx.xx.xx:2379",
+	//	}
+	//})
+	//micro.Selector(selector.NewSelector(func(options *selector.Options) {
+	//	options.Registry=reg
+	//}))
+	// New Service
+	reg := etcd.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{"127.0.0.1:2379","127.0.0.1:3379","127.0.0.1:4379"}//地址
+		//etcd2.Auth("gangan","123")//密码
+	})
+	service := micro.NewService(
+		micro.Registry(reg),
+		micro.Name("go.micro.srv.user"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	user.RegisterUserHandler(service.Server(), new(handler.User))
+	post.RegisterPostHandler(service.Server(), new(handler.Post))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
